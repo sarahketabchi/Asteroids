d60c4bec1d3fa7973223d3b72f246c17a8584760
Sean+Sarah <Sean+Sarah@appacademy.io>
2013-02-20 11:27:23 -0800

    End game when an asteroid hits the ship

--- app.js
+++ app.js
@@ -7,32 +7,40 @@ var AG = (function () {
     this.vel = vel;
 
     this.draw = function(ctx) {
-
+      randColor1 = 0;//Math.floor(Math.random() * 255);
+      randColor2 = 0;//Math.floor(Math.random() * 255);
+      randColor3 = 0;//Math.floor(Math.random() * 255);
       ctx.beginPath();
       ctx.arc(this.position[0], this.position[1],(this.size)/2 ,0,Math.PI*2,true);
-      ctx.fillStyle = "rgb(200,0,0)";
+      ctx.fillStyle = "rgba(" + randColor1 + "," + randColor2 + "," + randColor3 + " ,0.5)";
       ctx.fill();
     };
 
     this.update = function() {
       this.position[0] = this.position[0] + vel[0];
-      if ((this.position[0] < (0 + this.size/2)) || (this.position[0] > (500 - this.size/2))) {
-        vel[0] = vel[0] * -1;
-      };
       this.position[1] = this.position[1] + vel[1];
-      if ((this.position[1] < (0 + this.size/2)) || (this.position[1] > (500 - this.size/2))) {
-        vel[1] = vel[1] * -1;
-      };
+
+      if (this.position[0] < 0) {
+        this.position[0] = 500;
+      } else if (this.position[0] > 500) {
+        this.position[0] = 0;
+      }
+
+      if (this.position[1] < 0) {
+        this.position[1] = 500;
+      } else if (this.position[1] > 500) {
+        this.position[1] = 0;
+      }
     };
   }
 
   Asteroid.randomAsteroid = function() {
-    randSize = Math.floor(Math.random() * 20) + 20;
-    randX = Math.floor(Math.random() * (500 - randSize));
-    randY = Math.floor(Math.random() * (500 - randSize));
+    randSize = Math.floor(Math.random() * 50) + 50;
+    randX = Math.floor(Math.random() * (500));
+    randY = Math.floor(Math.random() * (500));
     var plusOrMinus = Math.random() < 0.5 ? -1 : 1;
-    velX = plusOrMinus * (Math.floor(Math.random() * 2) + 1);
-    velY = plusOrMinus * (Math.floor(Math.random() * 2) + 1);
+    velX = plusOrMinus * ((Math.random() * 2));
+    velY = plusOrMinus * ((Math.random() * 2));
     return asteroid = new Asteroid(randX, randY, randSize, [velX, velY]);
   }
 
@@ -42,22 +50,27 @@ var AG = (function () {
     var that = this;
 
     this.draw = function(ctx) {
+      ctx.beginPath();
+      ctx.arc(this.position[0], this.position[1],(this.size)/2 ,0,Math.PI*2,true);
       ctx.fillStyle = "rgb(0,200,0)";
-      ctx.fillRect (this.position[0], this.position[1], this.size, this.size);
+      ctx.fill();
     };
 
     this.isHit = function() {
+      var trueToggle = false;
       _.each(game.asteroids, function(asteroid) {
         var distance =  Math.sqrt(Math.pow((that.position[0] - asteroid.position[0]), 2) +
                         Math.pow((that.position[1] - asteroid.position[1]), 2))
-        if (distance < 20) {
-          console.log('YOU ARE DEAD');
+        if (distance < ((asteroid.size)/2) + (that.size)/2) {
+          trueToggle = true;
         }
       });
+      return trueToggle;
     }
   }
 
   var Game = function(ctx) {
+    var intervalID = undefined;
     var that = this;
     var ship = new Ship(that);
 
@@ -65,6 +78,7 @@ var AG = (function () {
     for(i = 0; i < 5; i ++) {
       this.asteroids.push(Asteroid.randomAsteroid());
     };
+
 
     this.draw = function() {
       ctx.clearRect(0,0,500,500);
@@ -78,17 +92,21 @@ var AG = (function () {
       _.each(this.asteroids, function(asteroid) {
         asteroid.update();
       });
-      ship.isHit();
       // ship.update();
     }
 
     this.updateDraw = function() {
       that.draw();
       that.update();
+
+      if (ship.isHit()) {
+        alert("You're dead!");
+        clearInterval(intervalID);
+      };
     }
 
     this.start = function(){
-      setInterval(this.updateDraw, FPS);
+      intervalID = setInterval(this.updateDraw, FPS);
     }
   }
 
@@ -116,6 +134,7 @@ $(function(){
   var ctx = canvas.get(0).getContext("2d");
 
   game = new AG.Game(ctx);
+  console.log(game.asteroids);
   game.start();
 
 
